d4a9de5341b8d23d8924c96859ffd52f2e698a04
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-11-09 21:39:09 +0300

    feat: реализует авторизацию пользователя

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
@@ -11,7 +16,10 @@ export class AuthService {
     private userService: UsersService,
     private jwtService: JwtService,
   ) {}
-  async login(userDto: CreateUserDto) {}
+  async login(userDto: CreateUserDto) {
+    const user = await this.validateUser(userDto);
+    return this.generateToken(user);
+  }
 
   async registration(userDto: CreateUserDto) {
     const candidate = await this.userService.getUserByEmail(userDto.email);
@@ -29,10 +37,22 @@ export class AuthService {
     });
     return this.generateToken(user);
   }
-  async generateToken(user: User) {
+  private async generateToken(user: User) {
     const payLoad = { email: user.email, id: user.id, roles: user.roles };
     return {
       token: this.jwtService.sign(payLoad),
     };
+  }
+
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    const passswordEquals = await bcrypt.compare(
+      userDto.password,
+      user.password,
+    );
+    if (user && passswordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({ message: 'Неверный логин или пароль' });
   }
 }
