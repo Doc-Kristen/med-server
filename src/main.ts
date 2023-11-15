eb41277107da5f22d9d54c1b042b87c495e40843
Кристина Мищенко <99183284+Doc-Kristen@users.noreply.github.com>
2023-11-15 17:10:42 +0300

    fix: исправляет настройки cors

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from './pipes/validation.pipe';
+import * as cors from 'cors';
 
 async function start() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
+  const allowedOrigins = ['http://localhost:5173'];
 
   const config = new DocumentBuilder()
     .setTitle('Сервер для обработки медицинских записей')
@@ -17,6 +19,18 @@ async function start() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/api/docs', app, document);
 
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+    }),
+  );
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
